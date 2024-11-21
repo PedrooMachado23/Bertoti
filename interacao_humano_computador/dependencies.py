a06f7cfeaf1253c8b667cb2e2585fe31675f5eb7
PedrooMachado23 <145769589+PedrooMachado23@users.noreply.github.com>
2024-11-21 10:31:18 -0300

    Create dependencies.py

---
+++ interacao_humano_computador/dependencies.py
@@ -0,0 +1,5 @@
+!pip install git+https://github.com/huggingface/parler-tts.git
+!pip install llama_cpp_python==0.2.90
+!pip install pytelegrambotapi
+!pip install -U openai-whisper
+!pip install pydub
