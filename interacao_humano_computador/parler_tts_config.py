cc31df85a3ad831de24b0fd90d9d625b0730c6ea
PedrooMachado23 <145769589+PedrooMachado23@users.noreply.github.com>
2024-11-21 10:32:18 -0300

    Create parler_tts_config.py

---
+++ interacao_humano_computador/parler_tts_config.py
@@ -0,0 +1,17 @@
+import torch
+from parler_tts import ParlerTTSForConditionalGeneration
+from transformers import AutoTokenizer
+import soundfile as sf
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+model = ParlerTTSForConditionalGeneration.from_pretrained("parler-tts/parler-tts-mini-v1").to(device)
+tokenizer = AutoTokenizer.from_pretrained("parler-tts/parler-tts-mini-v1")
+
+def convertToAudio(prompt, description):
+  input_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
+  prompt_input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+
+  generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
+  audio_arr = generation.cpu().numpy().squeeze()
+  sf.write("parler_tts_out.wav", audio_arr, model.config.sampling_rate)
